c233a753097633610c103be10e0da5651137958e
anilrnair03 <anilrnair03@gmail.com>
2026-01-05 15:37:21 +0530

    Update main.tf

--- modules/hosts/main.tf
+++ modules/hosts/main.tf
@@ -17,29 +17,9 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws",
-      version               = ">= 4.60.0",
-      configuration_aliases = [aws.pricing_calculator]
+      version               = ">= 4.60.0"
     }
   }
-}
-
-# provider "aws" {
-#   alias  = "cost_calculator"
-#   region = "us-east-1"
-# }
-
-locals {
-  pricing_os_map = {
-    "Red Hat Enterprise Linux" = "RHEL"
-    "Red Hat BYOL Linux"       = "RHEL",
-    "Ubuntu"                   = "Linux"
-    "Linux/UNIX"               = "Linux"
-    "Windows"                  = "Windows"
-  }
-  # pricing_arch_map = {
-  #   "x86_64" = "64-bit"
-  #   "arm64"  = "ARM64"
-  # }
 }
 
 data "aws_ami" "pvc_base" {
@@ -74,64 +54,4 @@ resource "aws_instance" "pvc_base" {
   tags = merge(var.tags, {
     Name = var.quantity == 0 ? var.name : format("%s-%02d", var.name, count.index + var.offset + 1)
   })
-
-  get_password_data           = var.instance_get_password_data
-  user_data                   = var.instance_user_data
-  user_data_replace_on_change = var.instance_replace_on_user_data_change
-}
-
-data "aws_pricing_product" "pvc_base" {
-  for_each = { for idx, instance in aws_instance.pvc_base : idx => instance }
-
-  provider = aws.pricing_calculator
-
-  service_code = "AmazonEC2"
-
-  filters {
-    field = "instanceType"
-    value = each.value.instance_type
-  }
-
-  filters {
-    field = "regionCode"
-    value = each.value.region
-  }
-
-  filters {
-    field = "operatingSystem"
-    value = local.pricing_os_map[data.aws_ami.pvc_base.platform_details]
-  }
-
-  dynamic "filters" {
-    for_each = var.pricing_license_model != null ? [1] : []
-    content {
-      field = "licenseModel"
-      value = var.pricing_license_model
-    }
-  }
-
-  # filters {
-  #     field = "processorArchitecture"
-  #     value = local.pricing_arch_map[data.aws_ami.pvc_base.architecture]
-  # }
-
-  filters {
-    field = "marketoption"
-    value = "OnDemand" # TODO Review marketoption as a variable to the module
-  }
-
-  filters {
-    field = "preInstalledSw"
-    value = "NA"
-  }
-
-  filters {
-    field = "tenancy"
-    value = "Shared"
-  }
-
-  filters {
-    field = "capacitystatus"
-    value = "Used"
-  }
 }
